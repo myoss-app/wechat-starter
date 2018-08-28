09431527dabd0d9568be643b421a7a2e8d41e463
Jerry.Chen <jerry.myoss@gmail.com>
2018-08-28 18:56:16 +0800

    feature: 增加微信核心自动配置

---
+++ wechat-starter-core/src/main/java/app/myoss/wechat/core/autoconfigure/WeChatCoreAutoConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018-2018 https://github.com/myoss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package app.myoss.wechat.core.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 微信核心自动配置
+ *
+ * @author Jerry.Chen
+ * @since 2018年8月28日 下午6:54:43
+ */
+@Configuration
+public class WeChatCoreAutoConfiguration {
+}
